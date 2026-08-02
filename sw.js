7b9a930bdbf97cb169b7f7485f96b9e5c812446c
snehanshuuu <snehansudeo@gmail.com>
2026-08-02 13:26:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,41 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('clock-it-v1').then((cache) => cache.addAll([
-      '/',
-      './manifest.json'
-    ]))
+const CACHE_NAME = 'clockit-cache-v2'; // Increment version number (v2, v3, etc.) on new updates
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force waiting service worker to become active immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Activate Event (Deletes old caches automatically)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // Delete old cached versions like v1
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Take control of all open app tabs immediately
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
